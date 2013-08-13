7482ef6e473d595b1663b5315979c6e11b7268bb
David Siew <davidsiew@outlook.com>
2013-08-13 17:12:20 +0100

    - Connecting to RabbitMQ
    https://github.com/mikehadlow/EasyNetQ/wiki/Connecting-to-RabbitMQ

---
+++ Playground/Publisher/Program.cs
@@ -0,0 +1,15 @@
+﻿using EasyNetQ;
+
+namespace Publisher
+{
+    class Program
+    {
+        static void Main()
+        {
+            using (var bus = RabbitHutch.CreateBus("host=Ubuntu-12"))
+            {
+                
+            }
+        }
+    }
+}
