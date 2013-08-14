50725a5d5a19cc7b0f17aeaaba6f65516909f8b5
David Siew <davidsiew@outlook.com>
2013-08-14 17:36:13 +0100

    - Add Subcriber to consume messages

---
+++ ENQ-Experimenting/SubscribeConsoleApp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using EasyNetQ;
+using PublishConsoleAppNamespace;
+
+namespace SubscribeConsoleAppNamespace
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("In subscriber");
+            using (var bus = RabbitHutch.CreateBus("host=Ubuntu-12"))
+            {
+                bus.Subscribe<MyMessage>("SubscribeConsoleAppId"
+                    , msg => Console.WriteLine(msg.Text));
+                Console.ReadKey();
+            }
+        }
+    }
+}
