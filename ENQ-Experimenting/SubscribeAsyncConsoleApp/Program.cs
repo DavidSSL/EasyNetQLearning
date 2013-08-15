65b071f88dd1272c494e05302b344feba498bfc1
David Siew <davidsiew@outlook.com>
2013-08-15 15:00:36 +0100

    - Little app demoing SubscribeAsync

---
+++ ENQ-Experimenting/SubscribeAsyncConsoleApp/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Net;
+using EasyNetQ;
+using PublishConsoleAppNamespace;
+using SubscribeWithLoggerConsoleAppNamespace;
+
+namespace SubscribeAsyncConsoleAppNamespace
+{
+    class Program
+    {
+        static void Main()
+        {
+            var logger = new MyLogger();
+            logger.InfoWrite("In subscriber");
+            var bus = RabbitHutch.CreateBus("host=Ubuntu-12");
+
+            bus.SubscribeAsync<MyMessage>("SubscribeAsyncConsoleAppId", msg =>
+                new WebClient().DownloadStringTaskAsync(new Uri("http://ubuntu-12/"))
+                .ContinueWith(task =>
+                    logger.InfoWrite("Received '{0}', Downloaded{1}", msg.Text, task.Result))
+                );
+        }
+    }
+}
