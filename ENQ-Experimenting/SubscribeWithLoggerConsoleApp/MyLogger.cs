e2f97bcca5937ef65c6395f8435f107f8039d9ab
David Siew <davidsiew@outlook.com>
2013-08-15 14:59:46 +0100

    - Demo of using NLog to output information to file instead of outputting information to console

---
+++ ENQ-Experimenting/SubscribeWithLoggerConsoleApp/MyLogger.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using EasyNetQ;
+using NLog;
+
+namespace SubscribeWithLoggerConsoleAppNamespace
+{
+    public class MyLogger:IEasyNetQLogger
+    {
+        private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
+        public void DebugWrite(string format, params object[] args)
+        {
+            Logger.Debug(string.Format(format, args));
+        }
+
+        public void InfoWrite(string format, params object[] args)
+        {
+            Logger.Info(string.Format(format, args));
+        }
+
+        public void ErrorWrite(string format, params object[] args)
+        {
+            Logger.Error(string.Format(format, args));
+        }
+
+        public void ErrorWrite(Exception exception)
+        {
+            Logger.ErrorException("An exception has occurred", exception);
+        }
+    }
+}
---
+++ ENQ-Experimenting/SubscribeWithLoggerConsoleApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using EasyNetQ;
+using PublishConsoleAppNamespace;
+
+namespace SubscribeWithLoggerConsoleAppNamespace
+{
+    class Program
+    {
+        static void Main()
+        {
+            var logger = new MyLogger();
+            logger.InfoWrite("In subscriber");
+            var bus = RabbitHutch.CreateBus("host=Ubuntu-12", x => x.Register<IEasyNetQLogger>(_ => logger));
+
+            bus.Subscribe<MyMessage>("SubscribeWithLoggerConsoleAppId"
+                                     , msg => logger.InfoWrite(msg.Text));
+        }
+    }
+}
